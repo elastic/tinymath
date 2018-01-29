15880941c43dc95e11e9956e6f8f8e798c78a2e4
Catherine Liu <catherine.liu@elastic.co>
2018-01-29 15:17:23 -0700

    Wrote abs function and unit tests

---
+++ test/functions/abs.spec.js
@@ -0,0 +1,17 @@
+import chai from 'chai';
+import { abs } from '../../src/functions/abs.js';
+
+chai.expect();
+const expect = chai.expect;
+
+describe('Abs', () => {
+  it('numbers', () => {
+    expect(abs(-10)).to.be.equal(10);
+    expect(abs(10)).to.be.eql(10);
+  });
+
+  it('arrays', () => {
+    expect(abs([-10, -20, -30, -40])).to.be.eql([10, 20, 30, 40]);
+    expect(abs([-13, 30, -90, 200])).to.be.eql([13, 30, 90, 200]);
+  });
+});
