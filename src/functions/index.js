d66959f7feae374cde65f81271e792f8e9824908
Catherine Liu <catherine.liu@elastic.co>
2018-01-30 11:12:35 -0700

    Added functions to index.js

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,11 +1,31 @@
-import {sum} from './sum';
-import {subtract} from './subtract';
-import {divide} from './divide';
-import {multiply} from './multiply';
+import { abs } from './abs';
+import { add } from './add';
+import { ceil } from './ceil';
+import { divide } from './divide';
+import { fix } from './fix';
+import { floor } from './floor';
+import { max } from './max';
+import { mean } from './mean';
+import { min } from './min';
+import { mod } from './mod';
+import { multiply } from './multiply';
+import { round } from './round';
+import { subtract } from './subtract';
+import { sum } from './sum';
 
 export const functions = {
-  sum,
-  subtract,
+  abs,
+  add,
+  ceil,
+  divide,
+  fix,
+  floor,
+  max,
+  mean,
+  min,
+  mod,
   multiply,
-  divide
+  round,
+  subtract,
+  sum
 };
