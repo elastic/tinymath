c94727e93ff60db8a5b164271c0fbf566d0ef523
Catherine Liu <catherine.liu@elastic.co>
2018-01-30 10:49:23 -0700

    Refactored mean function to use add function

--- src/functions/mean.js
+++ src/functions/mean.js
@@ -1,20 +1,11 @@
+import { add } from './add.js';
+
 export function mean(...args) {
-  if (args.length === 1) {
-    return args[0].reduce((result, current) => result + current) / args[0].length;
+  const result = add(...args);
+
+  if (Array.isArray(result)) {
+    return result.map((val => val / args.length));
   }
 
-  let totals = args.reduce((result, current) => {
-    if (Array.isArray(result) && Array.isArray(current)) {
-      if (current.length !== result.length) throw new Error('Matrix length mismatch');
-      return result.map((val, i) => val + current[i]);
-    }
-    if (Array.isArray(result)) return result.map(val => val + current);
-    if (Array.isArray(current)) return current.map(val => val + result);
-    return result + current;
-  });
-
-  if (Array.isArray(totals)) {
-    return totals.map((val => val / args.length));
-  }
-  return totals / args.length;
+  return result / args.length;
 };
