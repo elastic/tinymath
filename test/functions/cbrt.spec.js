bfcf3cc881e84c8b37da9a8aed9c1a352309db4c
Catherine Liu <catherine.liu@elastic.co>
2018-01-31 14:32:19 -0700

    Wrote cbrt function and unit tests

---
+++ test/functions/cbrt.spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { cbrt } from '../../src/functions/cbrt.js';
+
+describe('Cbrt', () => {
+  it('numbers', () => {
+    expect(cbrt(27)).to.be.equal(3);
+    expect(cbrt(-1)).to.be.equal(-1);
+  });
+
+  it('arrays', () => {
+    expect(cbrt([27, 64, 125])).to.be.eql([3, 4, 5]);
+    expect(cbrt([1, 8, 1000])).to.be.eql([1, 2, 10]);
+  });
+});
