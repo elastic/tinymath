4a62b3854ed019a0d2c26919ef92a5568ee8036d
Catherine Liu <catherine.liu@elastic.co>
2018-02-01 09:59:46 -0700

    Added phase 2 functions to functions/index.js

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,9 +1,13 @@
 import { abs } from './abs';
 import { add } from './add';
+import { cbrt } from './cbrt';
 import { ceil } from './ceil';
+import { cube } from './cube';
 import { divide } from './divide';
 import { fix } from './fix';
 import { floor } from './floor';
+import { log } from './log';
+import { log10 } from './log10';
 import { max } from './max';
 import { mean } from './mean';
 import { median } from './median';
@@ -11,8 +15,11 @@ import { min } from './min';
 import { mod } from './mod';
 import { mode } from './mode';
 import { multiply } from './multiply';
+import { pow } from './pow';
 import { range } from './range';
 import { round } from './round';
+import { sqrt } from './sqrt';
+import { square } from './square';
 import { subtract } from './subtract';
 import { sum } from './sum';
 import { unique } from './unique';
@@ -20,10 +27,14 @@ import { unique } from './unique';
 export const functions = {
   abs,
   add,
+  cbrt,
   ceil,
+  cube,
   divide,
   fix,
   floor,
+  log,
+  log10,
   max,
   mean,
   median,
@@ -31,8 +42,11 @@ export const functions = {
   mod,
   mode,
   multiply,
+  pow,
   range,
   round,
+  sqrt,
+  square,
   subtract,
   sum,
   unique,
