48c863cf5ce5dc90f0b1c11c4aa99ff1d75ffb7e
Catherine Liu <catherine.liu@elastic.co>
2018-02-14 16:02:56 -0700

    Added doc blocks for cube, pow, sqrt, and square

--- src/functions/cube.js
+++ src/functions/cube.js
@@ -1,4 +1,13 @@
 import { pow } from './pow.js';
+/**
+ * Calculates the cube of a number. For arrays, the function will be applied to each element.
+ * @param {(number|number[])} a a number or an array of numbers
+ * @return {(number|number[])} The cube of 'a' or an array with the cubes of each element.
+ *
+ * Examples:
+ * - cube(-3) returns -27
+ * - cube([3, 4, 5]) returns [27, 64, 125]
+ */
 export function cube(a) {
   return pow(a, 3);
 }
--- src/functions/sqrt.js
+++ src/functions/sqrt.js
@@ -1,3 +1,12 @@
+/**
+ * Calculates the square root of a number. For arrays, the function will be applied to each element.
+ * @param {(number|number[])} a a number or an array of numbers
+ * @return {(number|number[])} The square root of 'a' or an array with the square roots of each element.
+ *
+ * Examples:
+ * - sqrt(9) returns 3
+ * - sqrt([9, 16, 25]) returns [3, 4, 5]
+ */
 export function sqrt(a) {
   if (Array.isArray(a)) {
     return a.map(a => {
