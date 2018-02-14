c7f48ec750d0c13c235f80c34d1612189a620a3b
Catherine Liu <catherine.liu@elastic.co>
2018-02-14 15:29:42 -0700

    added doc block for cbrt

--- src/functions/cbrt.js
+++ src/functions/cbrt.js
@@ -1,3 +1,12 @@
+/**
+ * Calculates the cube root of a number. For arrays, the function will be applied to each element.
+ * @param {(number|number[])} a a number or an array of numbers
+ * @return {(number|number[])} The cube root of 'a' or an array with the cube roots of each element.
+ *
+ * Examples:
+ * - cbrt(-27) returns -3
+ * - cbrt([27, 64, 125]) returns [3, 4, 5]
+ */
 export function cbrt(a) {
   if (Array.isArray(a)) {
     return a.map(a => Math.cbrt(a));
