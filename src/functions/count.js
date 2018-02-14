ff978167233f2dbd17e0715be6606505393ffe79
Catherine Liu <catherine.liu@elastic.co>
2018-02-14 15:46:22 -0700

    Added doc blocks for count and size

--- src/functions/count.js
+++ src/functions/count.js
@@ -1,4 +1,13 @@
 import { size } from './size.js';
+/**
+ * Returns the length of an array. Alias for size
+ * @param {Array} a any array
+ * @return {(number)} the length of the array
+ *
+ * Examples:
+ * - count([]) returns 0
+ * - count([-1, -2, -3, -4]) returns 4
+ */
 export function count(a) {
   return size(a);
 }
--- src/functions/size.js
+++ src/functions/size.js
@@ -1,3 +1,12 @@
+/**
+ * Returns the length of an array. Alias for count
+ * @param {Array} a any array
+ * @return {(number)} the length of the array
+ *
+ * Examples:
+ * - size([]) returns 0
+ * - size([-1, -2, -3, -4]) returns 4
+ */
 export function size(a) {
   if (Array.isArray(a)) return a.length;
   throw new Error('Must pass an array');
