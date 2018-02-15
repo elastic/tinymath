49f0ccc27fe99f2995b4398e8ed5bdd1949f52c0
Catherine Liu <catherine.liu@elastic.co>
2018-02-15 09:48:09 -0700

    Added doc block for first and last

--- src/functions/first.js
+++ src/functions/first.js
@@ -1,3 +1,13 @@
+/**
+ * Returns the first element of an array. If anything other than an array is passed in, the input is returned.
+ * @param {*} a preferably an array but any input is accepted
+ * @return {*} first element of an array or the same input is returned
+ *
+ * Examples:
+ * - first(2) returns 2
+ * - first('foo') returns 'foo'
+ * - first([1, 2, 3]) returns 1
+ */
 export function first(a) {
   if (Array.isArray(a)) {
     return a[0];
--- src/functions/last.js
+++ src/functions/last.js
@@ -1,4 +1,13 @@
-export function last(a) {
+/**
+ * Returns the last element of an array. If anything other than an array is passed in, the input is returned.
+ * @param {*} a preferably an array but any input is accepted
+ * @return {*} last element of an array or the same input is returned
+ *
+ * Examples:
+ * - last(2) returns 2
+ * - last('foo') returns 'foo'
+ * - last([1, 2, 3]) returns 3
+ */ export function last(a) {
   if (Array.isArray(a)) {
     return a[a.length - 1];
   }
