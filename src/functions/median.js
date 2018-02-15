2f4244679a13100ea35a0da596c7ae8fd47e2738
Catherine Liu <catherine.liu@elastic.co>
2018-02-15 11:55:30 -0700

    Added doc block for median

--- src/functions/median.js
+++ src/functions/median.js
@@ -13,6 +13,18 @@ const findMedian = a => {
   return a[half];
 };
 
+/**
+ * Finds the median value(s) of all numbers passed into the function. If at least one array of numbers is passed into the function, the function will find the median by index.
+ * @param {...(number|number[])} args numbers and/or arrays of numbers
+ * @return {(number[]|number[][])} an array median value(s) of all numbers if all inputs are numbers or an array of arrays of median values of each index, including scalar numbers.
+ *
+ * Examples:
+ * - median(1, 1, 2, 3) returns 1.5
+ * - median(1, 1, 2, 2, 3) returns 2
+ * - median([10, 20, 30, 40], 10, 20, 30) returns [15, 20, 25, 25]
+ * - median([1, 9], 4, [3, 5]) returns [3, 4]
+ */
+
 export function median(...args) {
   if (args.length === 1) {
     if (Array.isArray(args[0])) return findMedian(args[0]);
