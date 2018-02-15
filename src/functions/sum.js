dbb8781c49c6e23d48044bf177ba840fdb03674e
Catherine Liu <catherine.liu@elastic.co>
2018-02-15 12:15:33 -0700

    Added doc block for sum

--- src/functions/sum.js
+++ src/functions/sum.js
@@ -1,5 +1,16 @@
 const findSum = (total, current) => total + current;
 
+/**
+ * Calculates the sum of all numbers and distinct values in arrays passed into the function.
+ * @param {...(number|number[])} args numbers and/or arrays of numbers
+ * @return {number} The sum of all numbers including distinct values in any arrays.
+ *
+ * Examples:
+ * - sum(1, 2, 3) returns 6
+ * - sum([10, 20, 30, 40], 10, 20, 30) returns 160
+ * - sum([1, 2], 3, [4, 5], 6) returns 21
+ */
+
 export function sum(...args) {
   return args.reduce((total, current) => {
     if (Array.isArray(current)) {
