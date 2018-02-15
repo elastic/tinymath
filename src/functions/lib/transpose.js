4cd374868b1d920a31d1ba1c567cf7fcf740e3a5
Catherine Liu <catherine.liu@elastic.co>
2018-02-15 11:38:37 -0700

    Cleaned up transpose

--- src/functions/lib/transpose.js
+++ src/functions/lib/transpose.js
@@ -5,9 +5,9 @@
  * @return {any[][]} transpose of args
  *
  * Examples:
- * - transpose([[1,2],[3,4],[5,6]], 0) returns [[1, 3, 5], [2, 4, 6]]
- * - transpose([10, 20, [10, 20, 30, 40], 30], 2) returns [[10, 10, 20, 30], [20, 10, 20, 30], [30, 10, 20, 30], [40, 10, 20, 30]]
- * - transpose([4,[1, 9], [3, 5]], 1) returns [[4, 1, 3], [4, 9, 5]]
+ * - transpose([[1,2], [3,4], [5,6]], 0) returns [[1, 3, 5], [2, 4, 6]]
+ * - transpose([10, 20, [10, 20, 30, 40], 30], 2) returns [[10, 20, 10, 30], [10, 20, 20, 30], [10, 20, 30, 30], [10, 20, 40, 30]]
+ * - transpose([4, [1, 9], [3, 5]], 1) returns [[4, 1, 3], [4, 9, 5]]
  */
 
 export function transpose(args, index) {
