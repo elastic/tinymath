8b2090407d0e22fbc7b5871ba40871c7b63b88a4
Joe Fleming <joe.fleming@gmail.com>
2018-03-12 18:16:28 -0700

    chore: remove node-resolve, consolidate outputs

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 /* eslint prefer-object-spread/prefer-object-spread: 0 */
-const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 const progress = require('rollup-plugin-progress');
@@ -20,17 +19,6 @@ const config = {
   input: 'src/index.js',
   plugins: [
     progress(),
-    resolve({
-      module: true,
-      jsnext: false,
-      browser: false,
-      preferBuiltins: true,
-    }),
-    // commonjs({
-    //   namedExports: {
-    //     'src/grammar.js': ['SyntaxError', 'parse'],
-    //   },
-    // }),
     commonjs(),
     babel({
       exclude: 'node_modules/**', // only transpile our source code
@@ -41,21 +29,20 @@ const config = {
 
 module.exports = [
   Object.assign({}, config, {
-    output: {
-      file: `${outputPath}/${filename}.mjs`,
-      format: 'es',
-      name: pkg.name,
-      banner,
-    },
-  }),
-  Object.assign({}, config, {
-    output: {
-      file: `${outputPath}/${filename}.js`,
-      format: 'umd',
-      name: pkg.name,
-      sourcemap: true,
-      banner,
-    },
+    output: [
+      {
+        file: `${outputPath}/${filename}.mjs`,
+        format: 'es',
+        name: pkg.name,
+        banner,
+      },
+      {
+        file: `${outputPath}/${filename}.js`,
+        format: 'umd',
+        name: pkg.name,
+        banner,
+      },
+    ],
   }),
   Object.assign({}, config, {
     output: {
